d0560aec5d5dce6cc038ad28483c20250d6300a1
katerinapatt <katerina.patticha@gmail.com>
2016-10-01 17:32:22 +0300

    Build script to seed the db from json file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+path = File.join(File.dirname(__FILE__), '../data/users.json')
+file = File.read(path)
+users_hash = JSON.parse(file)
+users = users_hash['users']
+
+users.each do |u| 
+	name = u['name']
+	picture = u['picture']
+	location = u['location']
+	user = User.create!( :gender => u['gender'], 
+				 :title => name['title'],
+				 :first_name => name['first'],
+				 :last_name => name['last'],
+				 :email => u['email'],
+				 :username => u['username'],
+				 :password => u['password'],
+				 :salt => u['salt'],
+				 :md5 => u['md5'],
+				 :sha1 => u['sha1'],
+				 :sha256 => u['sha256'],
+				 :registered => u['registered'],
+				 :dob => u['dob'],
+				 :phone => u['phone'],
+				 :cell => u['cell']	
+	)
+
+	location = Location.create!(:street =>  location['street'], 
+				:city =>  location['city'],
+				:state =>  location['state'],
+				:zip =>  location['zip'],
+				:user_id =>  user.id 
+	)
+
+	picture = Picture.create!( :user_id => user.id,
+				:large => picture['large'],
+				:medium => picture['medium'],
+				:thumbnail => picture['thumbnail']
+	)
+
+
+end
